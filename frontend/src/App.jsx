b66855d7483670858549ee4d1a929e60a167c63b
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-12-03 00:37:14 +0530

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>DeepRetrieve</h1>
+      <p>Multimodal RAG System</p>
+    </div>
+  )
+}
+
+export default App
